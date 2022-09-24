1edfde47aa6aa058dcb77f70f3ebb85f1bcdad0a
Caio Jordan <caioj2906@gmail.com>
2022-09-24 14:58:13 +0200

    using gpu

--- run_training.py
+++ run_training.py
@@ -37,7 +37,8 @@ if __name__ == '__main__':
                                  mode='max')
     lr_monitor = LearningRateMonitor()
 
-    trainer = Trainer(accelerator='cpu',
+    trainer = Trainer(accelerator='gpu',
+                      devices=1,
                       max_epochs=150,
                       val_check_interval=500,
                       callbacks=[model_ckpt, lr_monitor],
